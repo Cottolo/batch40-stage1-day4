857e19224484434f3e7d0de48ef969fb68d2c592
unknown <rahmat9654@gmail.com>
2022-09-17 10:58:06 +0700

    first commit

---
+++ project.js
@@ -0,0 +1,119 @@
+
+// let dataProject = []
+
+// function addProject(event){
+//     event.preventDeault()
+
+//     let projectName = document.getElementById("project-name")
+//     console.log(projectName);
+// }
+
+// let dataProject = []
+
+// function addProject(event){
+//     event.preventDefault()
+    
+//     let person = {
+//         name : "Rahmat",
+//         address : "Tangerang",
+//     }
+    
+//     console.log(person);
+//     dataProject.push(person)
+//     console.log(dataProject);
+// }
+
+let dataProject = []
+
+function addProject(event){
+    event.preventDefault()
+
+    let projectName = document.getElementById("project-name").value
+    let projectDescription = document.getElementById("description").value
+
+    let nodeJs = document.getElementById("node-js").checked
+    let nextJs = document.getElementById("next-js").checked
+    let reactJs = document.getElementById("react-js").checked
+    let typeScript = document.getElementById("typeScript").checked
+
+    let projectImage = document.getElementById("input-image").files
+
+    projectImage = URL.createObjectURL(projectImage[0])
+
+
+    if (nodeJs) {
+        nodeJs = document.getElementById("node-js").value
+    } else {nodeJs=``}
+    if (reactJs) {
+        reactJs = document.getElementById("react-js").value
+    } else {reactJs=``}
+    if (nextJs) {
+        nextJs = document.getElementById("next-js").value
+    } else {nextJs=``}
+    if (typeScript) {
+        typeScript = document.getElementById("typeScript").value
+    } else {typeScript=``}
+
+    let project = {
+        projectName,
+        projectDescription,
+        projectImage,
+        nodeJs,
+        reactJs,
+        nextJs,
+        typeScript,
+
+    }
+
+    dataProject.push(project)
+
+    // console.log(projectName);
+    // console.log(projectDescription);
+    // console.log(nodeJs);
+    // console.log(reactJs);
+    // console.log(nextJs);
+    // console.log(typeScript);
+    // console.log(projectImage);
+
+    console.log(dataProject);
+
+    
+
+    renderProject()
+}
+
+function renderProject(){
+
+    document.getElementById("container").innerHTML = ``
+
+    for (let index = 0; index < dataProject.length; index ++){
+    document.getElementById("container").innerHTML +=  `<a href="" class="container-post">
+
+    <div>
+        <div class="container-image">
+            <img src="${dataProject[index].projectImage}" alt="Project image">
+        </div>
+        <div class="container-title">
+            <h2>${dataProject[index].projectName}</h2>
+        </div>
+        <div >
+            <p class="container-duration">Durasi: 3 bulan</p>
+            <p class="container-description">${dataProject[index].projectDescription}</p>
+        </div>
+    </div>
+    <div class="container-icon">
+        <i class="${dataProject[index].nodeJs}"></i>
+        <i class="${dataProject[index].nextJs}"></i>
+        <i class="${dataProject[index].reactJs}"></i>
+        <i class="${dataProject[index].typeScript}"></i>
+
+    </div>
+    <div class="container-option">
+        <button class="container-option-left">edit</button>
+        <button class="container-option-right">delete</button>
+    </div>
+
+</a>
+`
+    }
+}
